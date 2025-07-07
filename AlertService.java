56258dd9f48c41d25c216521a212b40eef728b7f
einstein721 <einstein.ombati@strathmore.edu>
2025-07-07 19:10:51 +0300

    Update AlertService.java

--- AlertService.java
+++ AlertService.java
@@ -1,12 +1,3 @@
-// AlertService.java
 public interface AlertService {
     void sendAlert(String message);
-}
-
-// ConsoleAlertService.java
-public class ConsoleAlertService implements AlertService {
-    @Override
-    public void sendAlert(String message) {
-        System.out.println("ALERT: " + message);
-    }
 }
