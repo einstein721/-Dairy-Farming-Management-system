79af77b13b0ed9a85135966cc94cd983d2cbf009
einstein721 <einstein.ombati@strathmore.edu>
2025-07-08 11:04:14 +0300

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -2,26 +2,7 @@ public abstract class Animal {
     protected int animalId;
     protected String name;
 
-    public Animal(int animalId, String name) {
-        this.animalId = animalId;
-        this.name = name;
-    }
-
-    public int getAnimalId() {
-        return animalId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setAnimalId(int animalId) {
-        this.animalId = animalId;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    // Constructor, Getters, Setters...
 
     public abstract void displayDetails();
 }
